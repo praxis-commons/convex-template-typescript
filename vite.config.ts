69e5e8f7094e7b341a0e0eb4c1bd6704c99d7feb
kleyt0n <kleyton.vsc@gmail.com>
2026-03-08 22:34:19 +0000

    fix: increase the max-http-header-size

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 import viteReact from "@vitejs/plugin-react";
 
 export default defineConfig({
-  server: {
-    port: 3000,
-    hmr: {
-      // Avoid 431 Request Header Fields Too Large
-      path: "hmr",
-      protocol: "ws",
-    },
-  },
+  server: { port: 3000 },
   plugins: [
     tailwindcss(),
     tsConfigPaths({ projects: ["./tsconfig.json"] }),
